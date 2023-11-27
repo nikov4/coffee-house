8e924f86a1d76e5e8aa425fca5ebeb9ef9d20b85
Nikolai O <nik@o84.ru>
2023-11-27 18:14:44 +0300

    feat: add image scale effect for offers in menu page

--- coffee-house/scripts/scripts.js
+++ coffee-house/scripts/scripts.js
@@ -24,22 +24,25 @@ function showOffers(cat) {
     .then(function(data) {
       // fetching ok
       // console.log(data);
-      let item = '', img = '', pix = '', name = '', text = '', price = '';
+      let item = '', imgBlock = '', img = '', pix = '', name = '', text = '', price = '';
       let count = 0;
       for (let [key, value] of Object.entries(data)) {
         //console.log(key, value);
-        if (value.category === selectedCat){
+        if (value.category === selectedCat) {
           //console.log(value.name, value.description, value.price);
           //document.querySelector('.offers-container').textContent = `${value.name}, ${value.description}, ${value.price}`;
           item = document.querySelector('.offers-container').appendChild(document.createElement("div"));
           item.classList.add('offer-item');
-          img = item.appendChild(document.createElement("img"));
+          imgBlock = item.appendChild(document.createElement("div"));
+          imgBlock.classList.add('offer-img');
+          img = imgBlock.appendChild(document.createElement("img"));
           count += 1;
           pix = pix.concat('/images/', selectedCat, '-', count, '.jpg');
           img.setAttribute('src', pix);
           img.setAttribute('width', '680');
           img.setAttribute('height', '680');
           img.setAttribute('alt', `${value.name}`);
+          img.classList.add('offer-img');
           pix = '';
           name = item.appendChild(document.createTextNode(`${value.name}`));
           text = item.appendChild(document.createTextNode(`${value.description}`));
