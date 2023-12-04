d95e58cfa710849207e2f94d11637f8b94de2c83
Nikolai O <nik@o84.ru>
2023-12-04 23:11:02 +0300

    fix: clean some comments in code and styles

--- coffee-house/scripts/burger.js
+++ coffee-house/scripts/burger.js
@@ -4,13 +4,8 @@ let burger = document.querySelector('.burger-button');
 let burgerContainer = document.querySelector('.burger-container');
 
 burger.addEventListener('click', function() {
-  // let mainContainer = document.querySelector('.section-main');
-  // let footerContainer = document.querySelector('.section-contacts');
   burger.classList.toggle('burger__active');
-  // mainContainer.classList.toggle('hidden-area');
-  // footerContainer.classList.toggle('hidden-area');
   burgerContainer.classList.toggle('burger-container__open');
-  // console.log('click');
 });
 
 burgerContainer.addEventListener('click', function() {
